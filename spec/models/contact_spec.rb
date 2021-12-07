71ed7c84b61fb478b08e13b36c74ef7f23362700
Mateusz Jankowski <matijank10@gmail.com>
2021-12-07 12:13:30 +0100

    Add test for by_letter method in contact.rb and and improve readability

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,21 +1,25 @@
 require 'rails_helper'
 
 describe Contact do
+
   it "is invalid without a firstname" do
     contact = Contact.new(firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
+
   it "is invalid without a lastname" do
     contact = Contact.new(lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
+
   it "is invalid without an email address" do
     contact = Contact.new(email: nil)
     contact.valid?
     expect(contact.errors[:email]).to include("can't be blank")
   end
+
   it "is invalid with a duplicate email address" do
     Contact.create(
       firstname: 'Joe', lastname: 'Tester',
@@ -28,6 +32,7 @@ describe Contact do
     contact.valid?
     expect(contact.errors[:email]).to include("has already been taken")
   end
+
   it "is valid with a firstname, lastname and email" do
     contact = Contact.new(
       firstname: "Aron",
@@ -36,5 +41,47 @@ describe Contact do
     )
     expect(contact).to be_valid
   end
-  it "returns a contact's full name as a string"
+
+  it "returns a contact's full name as a string" do
+    contact = Contact.new(firstname: 'John', lastname: 'Doe', email: 'johndoe@emxaplme.com')
+    expect(contact.name).to eq 'John Doe'
+  end
+
+  it "returns a sorted array of results that match" do
+    smith = Contact.create(
+      firstname: "John",
+      lastname: "Smith",
+      email: "jsmith@emxaple.com"
+    )
+    jones = Contact.create(
+      firstname: "Tim",
+      lastname: "Jones",
+      email: "Tjones@emxaple.com"
+    )
+    johnson = Contact.create(
+      firstname: "John",
+      lastname: "Johnson",
+      email: "jjohnson@emxaple.com"
+    )
+    expect(Contact.by_letter("J")).to eq [johnson, jones]
+  end
+
+  it "omits results that do not match" do
+    smith = Contact.create(
+    firstname: 'John',
+    lastname: 'Smith',
+    email: 'jsmith@example.com'
+    )
+    jones = Contact.create(
+    firstname: 'Tim',
+    lastname: 'Jones',
+    email: 'tjones@example.com'
+    )
+    johnson = Contact.create(
+    firstname: 'John',
+    lastname: 'Johnson',
+    email: 'jjohnson@example.com'
+    )
+    expect(Contact.by_letter("J")).not_to include smith
+    end
 end
